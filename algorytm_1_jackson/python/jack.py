ff3c454949da387ff570d8fbd6db76088fdfcc69
Pawel Lampe <pawel.lampe@gmail.com>
2017-03-05 17:42:52 +0100

    fix

--- algorytm_1_jackson/python/jack.py
+++ algorytm_1_jackson/python/jack.py
@@ -4,8 +4,6 @@ linesWithJobs = sys.stdin.read().strip().split('\n')[1:]
 jobs = [j.split(' ') for j in linesWithJobs]
 jobs = [{'r': int(j[0]), 'p': int(j[1])} for j in jobs]
 
-# TODO: your code here
-
-cMax = 0
+cMax = -1
 
 print cMax
